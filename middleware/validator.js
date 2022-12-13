d3e66deabbe9f0c209f8dfbb98e1dd62ed7dd3e4
juan-francoa <esteban_rlt159@hotmail.com>
2022-12-13 12:40:16 -0500

    controller liquid and equipment

---
+++ middleware/validator.js
@@ -0,0 +1,14 @@
+const validator = (schema) => [
+    (req, res, next) =>{
+        const data = schema.validate(req.body, {abortEarly: false})
+        if(data.error){
+            return res.json({
+                success: false,
+                message: data.error.details.map(err => err.message)
+            })
+        }
+        next()
+    }
+]
+
+module.exports = validator
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ let router = require("express").Router()
 
 /* GET home page. */
 let user = require("./users")
-
+let liquid = require("./liquids")
+let equipment = require("./equipment")
 
 router.use("/auth", user)
-
+router.use("/liquids", liquid)
+router.use("/equiments", equipment)
 
 
 module.exports = router;
